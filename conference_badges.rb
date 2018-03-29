6a158aa9c610d28eab25b4425b7b925b87789959
Ally Netzel <netzelally@gmail.com>
2018-03-29 22:59:06 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -24,6 +24,5 @@ def assign_rooms(attendees)
 end
 
 def printer(attendees)
-  puts batch_badge_creator(attendees)
-  puts assign_rooms(attendees)
+  puts batch_badge_creator(assign_rooms(attendees))
 end
