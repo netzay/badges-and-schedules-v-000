64c9e19607d9726c737568a6164d94c8fcecc1f4
Ally Netzel <netzelally@gmail.com>
2018-03-29 22:23:38 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -15,8 +15,9 @@ end
 def assign_rooms(attendees)
   new = []
   rooms = [1,2,3,4,5,6,7]
-  attendees.each do |value|
+  attendees.each_with_index do |value, index|
     rooms.each do |room|
+      new += (index + room)
       new << "Hello, #{value}! You'll be assigned to room #{room}!"
     end
   end
