a2346c5698d7538f1131a2132359c5c414981299
Ally Netzel <netzelally@gmail.com>
2018-03-29 22:08:56 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,21 @@
-# Write your code here.
+
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(badges)
+  new = []
+  badges.each do |badge|
+    new << badge_maker(badge)
+  end
+  return new
+end
+
+
+def assign_rooms(attendees)
+  new = []
+  rooms = [1,2,3,4,5,6,7]
+  attendees.each_with_index do |value, index|
+    "Hello, #{value}! You'll be assigned to room #{index}!"
+  end
+end
