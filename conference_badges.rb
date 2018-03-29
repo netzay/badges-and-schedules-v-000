09708fea2e753cf51dc3510f5605fb614563c769
Ally Netzel <netzelally@gmail.com>
2018-03-29 23:27:34 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -27,7 +27,8 @@ def printer(attendees)
   puts batch_badge_creator(attendees)
   assign = assign_rooms(attendees)
     assign.each_entry do |line|
-      puts line.chomp
+      puts line.strip
+      
     end
   
 end
