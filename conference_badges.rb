92a6ab1045c409f3f5329e521910e728b3a205c3
Ally Netzel <netzelally@gmail.com>
2018-03-29 22:53:54 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -18,7 +18,7 @@ def assign_rooms(attendees)
   rooms = [1,2,3,4,5,6,7]
   attendees.each_with_index do |speaker, index|
     hash[speaker] = index
-    new << "Hello, #{speaker}! You'll be assigned to room #{rooms.each do room}!"
+    new << "Hello, #{speaker}! You'll be assigned to room #{rooms.each do |room|}!"
   end
   return new
 end
