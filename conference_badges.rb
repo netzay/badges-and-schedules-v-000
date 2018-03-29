009284e7f72b8a8ce8bc238c679968c0f79f6cf1
Ally Netzel <netzelally@gmail.com>
2018-03-29 22:28:31 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -16,7 +16,6 @@ def assign_rooms(attendees)
   new = []
   rooms = [1,2,3,4,5,6,7]
   attendees.each_with_index do |value, index|
-    new << (value, index)
-    puts new
+    puts (value, index)
 
 end
